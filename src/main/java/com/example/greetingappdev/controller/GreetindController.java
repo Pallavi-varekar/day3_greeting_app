689ea2a451e6717a4c7c19d8b052f93f311ffb7a
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-11-09 23:03:23 +0530

    added:print message using greetingcontroller

--- src/main/java/com/example/greetingappdev/controller/GreetindController.java
+++ src/main/java/com/example/greetingappdev/controller/GreetindController.java
@@ -1,4 +1,15 @@
 package com.example.greetingappdev.controller;
 
+import com.example.greetingappdev.model.Greeting;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GreetindController {
+    @PostMapping(value = "/body")//uc1
+    public String greetingMessages(@RequestBody Greeting greeting){
+        return greeting.getId()+" "+greeting.getName()+" "+greeting.getMessages();
+    }
 }
